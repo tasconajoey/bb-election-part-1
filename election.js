7d9accf4066ffed266730e783d07884190bc858e
tasconajoey <joey.tascona@gmail.com>
2017-02-17 17:30:00 -0500

    Finish full assignment

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
   // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData) {
+    for (var step = 0; step < responseData.candidates.length; step++) {
+      var name = responseData.candidates[step].name;
+      var votes = responseData.candidates[step].votes;
+      $('<li>').html(name + ' ' + votes).appendTo('#list');
+    }
+  });
 
 });
